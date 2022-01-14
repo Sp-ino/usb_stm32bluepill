dc907fa5f3ea466fc63ea1a46f9a6de056894910
sp-ino <spinovale97@gmail.com>
2022-01-14 21:04:36 +0100

    Update gitignore

--- usb_bulk_uart/host/repeater.py
+++ usb_bulk_uart/host/repeater.py
@@ -52,7 +52,7 @@ dev.set_configuration()
 
 
 #write initial data. It is an array of 128 bytes of value 0xFF
-initial_data = array.array('B', [0x30 for i in range(0,2*PACKET_SIZE_BYTES)])
+initial_data = array.array('B', [0x30 for i in range(0,PACKET_SIZE_BYTES)])
 try:
     writelen = dev.write(WRITE_EP, initial_data, timeout = USB_TIMEOUT)
 except: 
