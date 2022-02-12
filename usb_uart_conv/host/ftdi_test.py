eaf7a5123181a4b44abf31e9cfc1412e1b7ada28
sp-ino <spinovale97@gmail.com>
2022-02-12 19:18:45 +0100

    Add print_devices function

--- usb_uart_conv/host/ftdi_test.py
+++ usb_uart_conv/host/ftdi_test.py
@@ -10,16 +10,30 @@ Licensed under GNU license
 ftdi_test.py
 """
 
+from numpy import size
 import pyftdi.serialext as pser
 import pyftdi.ftdi as ftdi
 import serial.serialutil
 
 
+def print_devices():
+    possible_ftdi_devices = ftdi.Ftdi.list_devices()
+    print(f"\nFound {len(possible_ftdi_devices)} FTDI devices.")
+    for n, dev in enumerate(possible_ftdi_devices):
+        print(f"\nDevice {n}:")
+        for key, elem in zip(dev[0]._fields, dev[0]):
+            print(key, "=", elem)
+
+    print("\n")
+
+
 def main():
-    buffer = []
-    # print(ftdi.Ftdi.list_devices())
+    BAUDRATE = 38400
+
+    print_devices()
+
     try:
-        ftdi_dev = pser.serial_for_url("ftdi:///1", baudrate = 38400)
+        ftdi_dev = pser.serial_for_url("ftdi:///1", baudrate = BAUDRATE)
     except serial.serialutil.SerialException as e:
         raise OSError(e)
 
